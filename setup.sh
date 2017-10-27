54b1e1ba0e0d9e58fe13efc81df1f4a28b5ae607
Greg Schein <gregschein@gmail.com>
2017-10-27 14:05:56 -0700

    Fixed symlinks to not be shit

--- setup.sh
+++ setup.sh
@@ -2,19 +2,18 @@
 
 
 dotfile_root=`git rev-parse --show-cdup`
+my_path=`pwd`
+abs_path="$my_path$dotfile_root"
+echo $abs_path
 
-if [ -f ~/.profile ]; then 
-	ln -s "$dotfile_root/profile" ~/.profile
-fi
-if [ -f ~/.bash_profile ]; then 
-	ln -s "$dotfile_root/bash_profile" ~/.bash_profile
-fi
-if [ -f ~/.bash_aliases ]; then 
-	ln -s "$dotfile_root/bash_aliases" ~/.bash_aliases
-fi
-if [ -f ~/.bashrc ]; then 
-	ln -s "$dotfile_root/bashrc" ~/.bashrc
-fi
-if [ -f ~/.gitconfig ]; then 
-	ln -s "$dotfile_root/gitconfig" ~/.gitconfig
-fi
+
+rm ~/.profile
+ln -s "$abs_path/profile" ~/.profile
+rm ~/.bash_profile
+ln -s "$abs_path/bash_profile" ~/.bash_profile
+rm ~/.bash_aliases
+ln -s "$abs_path/bash_aliases" ~/.bash_aliases
+rm ~/.bashrc
+ln -s "$abs_path/bashrc" ~/.bashrc
+rm ~/.gitconfig
+ln -s "$abs_path/gitconfig" ~/.gitconfig
